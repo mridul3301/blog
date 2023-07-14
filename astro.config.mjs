07698f4d5166cfbf5a9f430353fade634f420cee
mridul3301 <mridulsharma3301@gmail.com>
2023-07-15 02:15:14 +0545

    updated

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,5 @@
 import { defineConfig } from 'astro/config'
 import sitemap from '@astrojs/sitemap'
-import react from '@astrojs/react'
 import image from '@astrojs/image'
 import tailwind from '@astrojs/tailwind'
 import addClasses from 'rehype-add-classes'
@@ -10,7 +9,6 @@ export default defineConfig({
   site: 'https://uses.craftz.dog/',
   integrations: [
     sitemap(),
-    react(),
     image({
       serviceEntryPoint: '@astrojs/image/sharp'
     }),
